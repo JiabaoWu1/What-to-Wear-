a010df11b7b2c15e5295bb5e305b8fae2689032e
JiabaoWu <wujiabaotonny@gmail.com>
2024-11-04 21:10:50 -0500

    new

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,13 +1,20 @@
 import "./Header.css";
 import headerLogo from "../../../assets/header__Logo.svg";
 import headerAvatar from "../../../assets/header__Avatar.svg";
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img src={headerLogo} alt="TripleTen logo" className="header__logo" />
 
       <p className="header__date-and-location">Date,location</p>
-      <button className="button header__add-clothes-btn"> + Add clothes</button>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="button header__add-clothes-btn"
+      >
+        {" "}
+        + Add clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Terrence T</p>
         <img src={headerAvatar} alt="Terrence T" className="header__avatar" />
