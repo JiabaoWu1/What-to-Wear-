7b6f6822b88300cc1de3627614a5fa508eac0ea9
JiabaoWu <wujiabaotonny@gmail.com>
2024-11-19 20:40:52 -0500

    new

--- src/components/App/ItemModal/ItemModal.jsx
+++ src/components/App/ItemModal/ItemModal.jsx
@@ -9,7 +9,7 @@ function ItemModal({ isOpen, onClose, card }) {
         </button>
         <img src={card.link} alt="" className="modal__image" />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
+          <p className="modal__caption">{card.name}</p>
           <p className="modal__weather">Weather: {card.weather}</p>
         </div>
       </div>
