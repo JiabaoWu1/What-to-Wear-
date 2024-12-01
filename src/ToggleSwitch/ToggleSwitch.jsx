514ffc17760cd3de5dd2a02276054409407ed980
JiabaoWu <wujiabaotonny@gmail.com>
2024-12-01 15:04:40 -0500

    mew

--- src/ToggleSwitch/ToggleSwitch.jsx
+++ src/ToggleSwitch/ToggleSwitch.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./ToggleSwitch.css";
 
 const ToggleSwitch = () => {
-  return <div>Toggle Switch</div>;
+  const [currentTempUnit, handleToggleSwitchChange] = useState("C");
+  const handleChange = (e) => {
+    if (currentTempUnit === "C") {
+      handleToggleSwitchChange("F");
+    } else {
+      handleToggleSwitchChange("C");
+    }
+  };
+
+  return (
+    <label htmlFor="" className="switch">
+      <input type="checkbox" className="switch__box" onChange={handleChange} />
+      <span
+        className={
+          currentTempUnit === "F"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider switch__slider-C"
+        }
+      ></span>
+      <p>F</p>
+      <p>C</p>
+    </label>
+  );
 };
 
 export default ToggleSwitch;
