f4f4728cbf35949e5cb3349519342675cca8c4a7
JiabaoWu <wujiabaotonny@gmail.com>
2024-12-13 17:01:18 -0500

    new

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,7 +1,7 @@
 import "./Header.css";
 import headerLogo from "../../../assets/header__Logo.svg";
 import headerAvatar from "../../../assets/header__Avatar.svg";
-import ToggleSwitch from "../../../ToggleSwitch/ToggleSwitch";
+import ToggleSwitch from "../../ToggleSwitch/ToggleSwitch";
 import { Link } from "react-router-dom";
 
 function Header({ handleAddClick, weatherData }) {
