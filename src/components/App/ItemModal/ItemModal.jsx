31b9297aca53f63fbb14250e20564683ed76baa9
JiabaoWu <wujiabaotonny@gmail.com>
2024-12-27 20:09:39 -0500

    new

--- src/components/App/ItemModal/ItemModal.jsx
+++ src/components/App/ItemModal/ItemModal.jsx
@@ -1,6 +1,18 @@
 import "./ItemModal.css";
+import React from "react";
+import { deleteItem } from "../../../utils/api";
 
-function ItemModal({ isOpen, onClose, card }) {
+function ItemModal({ isOpen, onClose, onDelete, card }) {
+  if (!isOpen) return null;
+
+  const handleDelete = () => {
+    if (!card || !card._id) {
+      console.error("Item is missing _id", card);
+      return;
+    }
+
+    deleteItem(card);
+  };
   return (
     <div className={`modal ${isOpen && "modal_opened"}`}>
       <div className="modal__content modal__content_type_image">
@@ -9,10 +21,14 @@ function ItemModal({ isOpen, onClose, card }) {
           type="button"
           className="modal__close"
         ></button>
+
         <img src={card.imageUrl} alt="Modal Image" className="modal__image" />
         <div className="modal__footer">
           <p className="modal__caption">{card.name}</p>
           <p className="modal__weather">Weather: {card.weather}</p>
+          <button className="modal__delete-button" onClick={handleDelete}>
+            Delete Card
+          </button>
         </div>
       </div>
     </div>
